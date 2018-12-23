8900f913a14e1f654aca6c620e6d5a16cfec2804
Artem <artemautonomia@gmail.com>
2018-12-23 23:15:09 +0200

    added new rules in urlManager

--- frontend/config/main.php
+++ frontend/config/main.php
@@ -41,6 +41,13 @@ return [
             'enablePrettyUrl' => true,
             'showScriptName' => false,
             'rules' => [
+                '' => 'site/index',
+                'contact' => 'contact/index',
+                '<_a:login|logout>' => 'site/<_a>',
+                '<_c:[\w\-]+>' => '<_c>/index',
+                '<_c:[\w\-]+>/<id:\d+>' => '<_c>/view',
+                '<_c:[\w\-]+>/<_a:[\w-]+>' => '<_c>/<_a>',
+                '<_c:[\w\-]+>/<id:\d+>/<_a:[\w\-]+>' => '<_c>/<_a>',
             ],
         ],
 
