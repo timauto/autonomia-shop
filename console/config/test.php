8e9159150b9773e301c9a41b87fe7dac3613327d
Artem <artemautonomia@gmail.com>
2018-12-23 12:32:27 +0200

    first commit

---
+++ console/config/test.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: User
+ * Date: 23.12.2018
+ * Time: 12:28
+ */
+
+return [
+    'components' => [
+        'request' => [
+            'cookieValidationKey' => new \yii\helpers\UnsetArrayValue(),
+        ],
+    ],
+];
